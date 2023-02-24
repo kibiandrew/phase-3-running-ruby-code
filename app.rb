08070aaf201471c656fe77880fe8a3fcfff2130d
Andrew Kibiwott <kibiandrew@gmail.com>
2023-02-24 09:30:10 +0300

    done

--- app.rb
+++ app.rb
@@ -1,12 +1,12 @@
-# pp [{ id: 1, hello: "World" }, 
-#     { id: 2, hello: "Ruby" }, 
-#     { id: 3, hello: "Moon" }, 
-#     { id: 4, hello: "Learner" }]
+pp [{ id: 1, hello: "World" }, 
+    { id: 2, hello: "Ruby" }, 
+    { id: 3, hello: "Moon" }, 
+    { id: 4, hello: "Learner" }]
 
-# p [1,2,3]
-# puts "Hello World!"
-# print "Pass this test, please."
-# p [1,2,3]
+p [1,2,3]
+puts "Hello World!"
+print "Pass this test, please."
+p [1,2,3]
 
 # def add
     
@@ -21,3 +21,10 @@ def add num1, num2
 end
 
 puts add(1,3) + add(2,2)
+puts "Hello world!"
+puts "Hello world!"
+puts "Hello world!"
+puts "34"
+print "Hello world!"
+print "Hello world!"
+print "Hello world!"
